7980f4a1c368388956af62ef4dd14c27bf397ba7
kerkir02 <kerkir@vp.pl>
2026-02-15 23:01:56 +0100

    added thirst enemy
    added enemy
    animation for enemy and collision with player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,14 +6,18 @@ public class PlayerMovement : MonoBehaviour
     private Animator playerAnimator;
     private SpriteRenderer playerSR;
 
-    [SerializeField] private float speed = 5;
-    [SerializeField] private float jumpForce = 5;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private int lives = 5;
+    [SerializeField] private float hitPower = 5f;
+    [SerializeField] private float hitTime = 0.1f;
 
     private float verticalInput;
     private float horizontalInput;
     private bool isInMove;
     private bool spaceDown;
     private bool isOnGround;
+    private bool isHit;
     private int jumpNumber;
     private Vector2 v;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -24,6 +28,7 @@ public class PlayerMovement : MonoBehaviour
         playerSR = GetComponent<SpriteRenderer>();
         spaceDown = false;
         jumpNumber = 0;
+        playerSR.enabled = true;
     }
 
     // Update is called once per frame
@@ -71,7 +76,9 @@ public class PlayerMovement : MonoBehaviour
         isInMove = horizontalInput != 0f;
         playerAnimator.SetBool("isInMove", isInMove);
         playerAnimator.SetBool("isOnGround", isOnGround);
+        playerAnimator.SetBool("isHit", isHit);
 
+        //turn player
         if (horizontalInput > 0f)
         {
             playerSR.flipX = false;
@@ -84,6 +91,24 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        //collision with enemy
+        if (collision.collider.CompareTag("Enemy"))
+        {
+            if (isHit)
+            {
+                return;
+            }
+            //check is jumped on his head
+            foreach (ContactPoint2D contact in collision.contacts)
+            {
+                if (contact.normal.y > 0.5f)
+                {
+                    Destroy(collision.collider.gameObject);
+                    return;
+                }
+            }
+            GetHit(collision.transform);           
+        }
         if (collision.collider.CompareTag("Ground"))
         {
             foreach (ContactPoint2D contact in collision.contacts)
@@ -95,6 +120,48 @@ public class PlayerMovement : MonoBehaviour
                     break;
                 }
             }
+        }
+    }
+
+    private void GetHit(Transform enemy)
+    {
+        Vector2 knockback = new Vector2(Mathf.Sign(transform.position.x - enemy.position.x), 1f);
+
+        playerRB.linearVelocity = Vector2.zero;
+        playerRB.linearVelocity = knockback * hitPower;
+
+        lives--;
+        isHit = true;
+        isOnGround = false;
+        Invoke(nameof(GetUnHit), 2f);
+        UnFlashing();
+    }
+
+    private void GetUnHit()
+    {
+        CancelInvoke(nameof(GetUnHit));
+        CancelInvoke(nameof(UnFlashing));
+        CancelInvoke(nameof(Flashing));
+        isHit = false;
+        playerSR.enabled = true;
+    }
+    //flashing with sprite renderer
+    private void Flashing()
+    {
+        CancelInvoke(nameof(Flashing));
+        playerSR.enabled = true;
+        if (isHit)
+        {
+            Invoke(nameof(UnFlashing), hitTime);
+        }
+    }
+    private void UnFlashing()
+    {
+        CancelInvoke(nameof(UnFlashing));
+        playerSR.enabled = false;
+        if (isHit)
+        {
+            Invoke(nameof(Flashing), hitTime);
         }
     }
 }
