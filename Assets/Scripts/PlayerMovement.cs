fe78ed497021303b6059b9d39ce5c837b81ead91
kerkir02 <kerkir@vp.pl>
2026-02-15 19:30:03 +0100

    player double jump mechanic and animation
    player double jump mechanic and animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,16 +7,23 @@ public class PlayerMovement : MonoBehaviour
     private SpriteRenderer playerSR;
 
     [SerializeField] private float speed = 5;
+    [SerializeField] private float jumpForce = 5;
 
     private float verticalInput;
     private float horizontalInput;
     private bool isInMove;
+    private bool spaceDown;
+    private bool isOnGround;
+    private int jumpNumber;
+    private Vector2 v;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         playerRB = GetComponent<Rigidbody2D>();
         playerAnimator = GetComponent<Animator>();
         playerSR = GetComponent<SpriteRenderer>();
+        spaceDown = false;
+        jumpNumber = 0;
     }
 
     // Update is called once per frame
@@ -25,22 +32,45 @@ public class PlayerMovement : MonoBehaviour
         //verticalInput = Input.GetAxis("Vertical");
         horizontalInput = Input.GetAxisRaw("Horizontal");
         PlayerAnimationMove();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            spaceDown = true;
+        }
     }
 
     void FixedUpdate()
     {
         PlayerMove();
+        if (spaceDown && jumpNumber < 2)
+        {
+            PlayerJump();
+        }
+    }
+
+    private void PlayerJump()
+    {
+        v = playerRB.linearVelocity;
+        v.y = jumpForce;
+        playerRB.linearVelocity = v;
+
+        spaceDown = false;
+        isOnGround = false;
+        jumpNumber++;
     }
 
     private void PlayerMove()
     {
-        playerRB.linearVelocity = new Vector2(horizontalInput * speed, playerRB.linearVelocity.y);
+        v = playerRB.linearVelocity;
+        v.x = horizontalInput * speed;
+        playerRB.linearVelocity = v;
     }
 
     private void PlayerAnimationMove()
     {
         isInMove = horizontalInput != 0f;
         playerAnimator.SetBool("isInMove", isInMove);
+        playerAnimator.SetBool("isOnGround", isOnGround);
 
         if (horizontalInput > 0f)
         {
@@ -49,6 +79,22 @@ public class PlayerMovement : MonoBehaviour
         else if (horizontalInput < 0f)
         {
             playerSR.flipX = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Ground"))
+        {
+            foreach (ContactPoint2D contact in collision.contacts)
+            {
+                if (contact.normal.y > 0.5f)
+                {
+                    isOnGround = true;
+                    jumpNumber = 0;
+                    break;
+                }
+            }
         }
     }
 }
