4b37a0588901fd0a0ee400c8a73a5e51411e4767
kerkir02 <kerkir@vp.pl>
2026-03-05 18:42:20 +0100

    level 2 finished
    level 2 finished:
    set enemies, background, collectibles, objectives and instructions

--- Assets/Scripts/Collectibles/GreenGem.cs
+++ Assets/Scripts/Collectibles/GreenGem.cs
@@ -5,7 +5,7 @@ public class GreenGem : Collectibles
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        value = 10;
+        value = 25;
         type = CollectType.Score;
     }
 }
